47da8d5f2e11c0fdfe62b8eb909b3ce2575112f6
mbauskar <mbauskar@gmail.com>
2015-11-06 18:42:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## HelpDesk
+
+helpdesk
+
+#### License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(
+    name='helpdesk',
+    version=version,
+    description='helpdesk',
+    author='helpdesk',
+    author_email='makarand.b@indictranstech.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=("frappe",),
+)
