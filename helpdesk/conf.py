7569c8ebcc4d396e48459a7fc2afc96d8e270b1d
mbauskar <mbauskar@gmail.com>
2015-11-18 12:29:16 +0530

    API Request parameters schema

--- helpdesk/conf.py
+++ helpdesk/conf.py
@@ -16,16 +16,174 @@
 api_request_schema = {
 	"login":{
 		"fields": {
-			"email":{
+			"user":{
 				"is_mandatory": 1,
 				"length": 35,
-				"type": "string"
+				"type": basestring
 			},
 			"password":{
 				"is_mandatory": 1,
 				"length": 15,
-				"type": "string"
+				"type": basestring
 			}
 		},
-	}
+	},
+	"reportIssue":{
+		"fields":{
+			"subject": {
+				"is_mandatory": 1,
+				"length": 200,
+				"type": basestring
+			},
+			"description": {
+				"is_mandatory": 1,
+				"length": 400,
+				"type": basestring
+			},
+			"user": {
+				"is_mandatory": 1,
+				"length": 35,
+				"type": basestring
+			},
+			"department": {
+				"is_mandatory": 1,
+				"length": 30,
+				"type": basestring
+			},
+			"sid": {
+				"is_mandatory": 1,
+				"length": 60,
+				"type": basestring
+			}
+		}
+	},
+	"getIssueStatus": {
+		"fields": {
+			"ticket_id": {
+				"is_mandatory": 1,
+				"length": 20,
+				"type": basestring
+			},
+			"user": {
+				"is_mandatory": 1,
+				"length": 35,
+				"type": basestring
+			},
+			"sid": {
+				"is_mandatory": 1,
+				"length": 60,
+				"type": basestring
+			}
+		}
+	},
+	"getIssueList": {
+		"fields": {
+			"user": {
+				"is_mandatory": 1,
+				"length": 35,
+				"type": basestring
+			},
+			"sid": {
+				"is_mandatory": 1,
+				"length": 60,
+				"type": basestring
+			},
+			"filter": {
+				"type": dict,
+				"options": [
+						"resolution_date", "user", "subject", 
+						"ticket_id", "department",
+						"status", "opening_date"
+					],
+				"default_option": "name",
+				"allowed_operations": ["Like", "In", "Not In", "!=", "=", ">", "<", ">=", "<="],
+				"default_operation": "="
+			},
+			"order_by": {
+				"type": basestring,
+				"options": ["ASC", "DESC"],
+				"default": "DESC"
+			},
+			"sort_by": {
+				"type": basestring,
+				"length": 20,
+				"default": "name",
+				"options": ["name", "subject", "opening_date", "modified"]
+			},
+			"limit": {
+				"type": int,
+				"default": 20,
+				"max_value": 50,
+			}
+		}
+	},
+	"updateIssue":{
+		"fields":{
+			"user": {
+				"is_mandatory": 1,
+				"length": 35,
+				"type": basestring
+			},
+			"ticket_id": {
+				"is_mandatory": 1,
+				"length": 20,
+				"type": basestring
+			},
+			"subject": {
+				"length": 200,
+				"type": basestring
+			},
+			"description": {
+				"length": 400,
+				"type": basestring
+			},
+			"department": {
+				"length": 30,
+				"type": basestring
+			},
+			"sid": {
+				"is_mandatory": 1,
+				"length": 60,
+				"type": basestring
+			}
+		}
+	},
+	"getIssueHistory": {
+		"fields": {
+			"user": {
+				"is_mandatory": 1,
+				"length": 35,
+				"type": basestring
+			},
+			"ticket_id": {
+				"is_mandatory": 1,
+				"length": 20,
+				"type": basestring
+			},
+			"sid": {
+				"is_mandatory": 1,
+				"length": 60,
+				"type": basestring
+			}
+		}
+	},
+	# "deleteIssue": {
+	# 	"fields": {
+	# 		"ticket_id": {
+	# 			"is_mandatory": 1,
+	# 			"length": 20,
+	# 			"type": basestring
+	# 		},
+	# 		"user": {
+	# 			"is_mandatory": 1,
+	# 			"length": 35,
+	# 			"type": basestring
+	# 		},
+	# 		"sid": {
+	# 			"is_mandatory": 1,
+	# 			"length": 60,
+	# 			"type": basestring
+	# 		}
+	# 	}
+	# },
 }
