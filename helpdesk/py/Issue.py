53f05c19f006e90b2f103b7e38c96f6931afe980
mbauskar <mbauskar@gmail.com>
2015-12-08 13:05:51 +0530

    [fixes] removed Issue.py, added permissions query for Department Head

--- helpdesk/py/Issue.py
+++
@@ -1,5 +0,0 @@
-
-import frappe
-
-def get_permission_query_conditions(user):
-	pass
--- helpdesk/py/issue.py
+++ helpdesk/py/issue.py
@@ -5,6 +5,16 @@ def get_permission_query_conditions(user):
 
 	if "System Manager" in frappe.get_roles(user):
 		return None
+	elif "Department Head" in frappe.get_roles(user):
+		return """\
+		(tabIssue.owner = '{user}' or tabIssue.raised_by = '{user}') and (tabIssue.department = '{dept}')
+		or (tabIssue.name in (select tabToDo.reference_name from tabToDo where
+			(tabToDo.owner = '{user}' and tabToDo.status = 'Open') or tabToDo.assigned_by = '{user}'
+			and tabToDo.reference_type = 'Issue' and tabToDo.reference_name=tabIssue.name))\
+		""".format(
+				user=frappe.db.escape(user),
+				dept=frappe.db.get_value("User", user, "department")
+			)
 	else:
 		return """\
 		(tabIssue.owner = '{user}' or tabIssue.raised_by = '{user}') 
